5462cbcbcd6355c620864df8c66aa1af70b406ba
Jack Anderson <40208539@live.napier.ac.uk>
2017-07-18 19:21:15 +0100

    Added syncronous timeouts for my fancy intro

--- js/terminal.js
+++ js/terminal.js
@@ -1,19 +1,61 @@
-$( document ).ready(function(){
-	console.log("READY!");
-	//Do the sequence here !
-	function typeText(text, interval){
-		console.log("Called function");
-		for (var i = 0; i < text.length; i++) {
-			doTimeout(text[i], i, interval);
+function typeText(text, interval, callback){
+	for (var i = 0; i < text.length; i++) {
+		if(i == text.length - 1){
+			doTimeout(text[i], i, interval, callback);
+		}else{
+			doTimeout(text[i], i, interval)
 		}
 	}
+}
 
-	function doTimeout(text, i, interval){
-		setTimeout(function(){
-			console.log(text);
-			$("#terminal").append(text);
-		},i * interval);
+function doTimeout(text, i, interval, callback){
+	setTimeout(function(){
+		$("#terminal-cursor").before(text);
+		if(callback !== undefined){
+			callback();
+		}
+	},i * interval);
+}
+
+function popFirst(array) {
+	//Remove first item from array and return it.
+	if(array !== undefined){
+		var item = array[0];
+		array.splice(0, 1);
+		console.log(array);
+		return item;
 	}
+	return null;
+}
 
-	typeText("This is a test", 100);
+//The queue is the sequence of timed events that take place, in order.
+var queue = [
+	function(callback){
+		console.log("i ran!");
+		typeText("First line!", 75, callback);
+	},
+	function(callback){
+		console.log("me too !");
+		typeText("Second line!", 75, callback);
+	}
+];
+
+function runQueue(array){
+	//Runs the first item in the array and passes it
+	//the run function so that it can in turn keep
+	//calling items to be run.
+	var toRun = popFirst(array);
+	if(toRun == null){
+		return;	
+	}
+	toRun(function(){
+		runQueue(array);
+	});
+}
+
+$( document ).ready(function(){
+	//Opening Sequence
+	$("#terminal-cursor").before("<span style='color:green'>root@192.168.0.1:~$ </span>");
+	console.log(queue);
+	runQueue(queue);
 });
