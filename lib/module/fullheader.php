6bd23c47ccc053427b650bc528ac7081906d09bb
Jack Anderson <40208539@live.napier.ac.uk>
2017-07-21 10:17:12 +0100

    breaking up file into modules

---
+++ lib/module/fullheader.php
@@ -0,0 +1,15 @@
+<div class="full-header">
+	<div class="site-text">
+		<div class="site-text-wrapper">
+		<div class="text-wrapper">
+			<div class="site-title">JACK ANDERSON</div>
+			<div class="site-description">I'm a 21 year old software engineering student based in Edinburgh, Scotland.<p class="terminal-cursor">▮</p></div>
+		</div>
+		<a id="down-link">
+			<span class="glyphicon glyphicon-arrow-down"></span>
+		</a>
+		</div>
+	</div>
+</div>
+
+
---
+++ lib/module/head.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="lib/css/main.css">
+		<link href="https://fonts.googleapis.com/css?family=Oxygen+Mono" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script type="text/javascript" src="lib/js/scroll.js"></script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
