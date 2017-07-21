b63f688e22335d3da2bb9285a674966a34c4f047
Jack Anderson <40208539@live.napier.ac.uk>
2017-07-21 22:41:14 +0100

    projects page

---
+++ lib/module/smallheader.php
@@ -0,0 +1,4 @@
+<div class="small-header">
+<div class="small-header-text">JACK ANDERSON.</div>
+</div>
+
---
+++ projects.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<?php include 'lib/module/head.php' ?>
+		<title>Jack Anderson - Projects</title>
+	</head>
+	<body>
+		<?php include 'lib/module/smallheader.php' ?>
+		<div class="container">
+			<div class="content">
+				<div class="row">
+					<div class="col-sm-3 col-xs-12"><h3 class="page-title">PROJECTS.</h3></div>
+					<div class="col-sm-7 col-xs-12">
+						<div class="row">
+							<div class="col-sm-6 col-xs-12">Project goes here.</div>
+							<div class="col-sm-6 col-xs-12">Project goes here.</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-6 col-xs-12">Project goes here.</div>
+							<div class="col-sm-6 col-xs-12">Project goes here.</div>
+						</div>
+					</div>
+					<div class="col-sm-2 col-xs-12 links">
+						<?php include 'lib/module/nav.php' ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
